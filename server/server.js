72fbab7634d2f3f9a53778541fe4199ee1905b5f
RickieSTS <sysgames.77@gmail.com>
2025-02-15 16:06:49 +0000

    feat(server): add tls certificate path and changed connection to be https

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const dotenv = require('dotenv').config()
 const express = require("express");
+const https = require("https")
+const path = require("path")
+const fs = require("fs")
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT_HTTPS || 3001;
+
+const tlsServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
 
 app.use(require("./routes/v1/routes"))
 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-  });
-  
+tlsServer.listen(PORT, () => {
+  console.log(`https://localhost:${PORT}`);
+});
